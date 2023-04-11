ef2c842515677216e9c6ea4a985add1239b98860
devanshbhatt05 <96202097+devanshbhatt05@users.noreply.github.com>
2023-04-11 09:10:01 +0530

    Add files via upload

---
+++ reference/event_demo.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+
+
+// create class
+class MyEventEmitter extends EventEmitter {}
+
+// object
+const myEvent = new MyEventEmitter();
+
+
+myEvent.on('event' , () => console.log('event trigger'));
+
+myEvent.emit('event')
